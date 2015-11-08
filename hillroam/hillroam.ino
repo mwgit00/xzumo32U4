a3fcf38fba7fe44d873693affccf79f87a35a1db
Mark Whitney <mw0868@earthlink.net>
2015-11-08 12:29:42 -0600

    New test mode.
    Button B activates mode to show calibrated accelerometer readings.  Got
    rid of max-min stuff.  Now use threshold counts for logic.

--- hillroam/hillroam.ino
+++ hillroam/hillroam.ino
@@ -1,7 +1,5 @@
 // Pololu Zumo 32U4 Arduino Sketch
 //
-// (adapted from InertialSensors example)
-//
 // This program tries to keep the robot roaming around on a hilly surface.
 // If it starts pointing downhill, it will try to back up and rotate
 // to point uphill before roaming again.
@@ -11,6 +9,7 @@
 
 Zumo32U4LCD lcd;
 Zumo32U4ButtonA buttonA;
+Zumo32U4ButtonB buttonB;
 Zumo32U4Motors motors;
 
 LSM303 compass;
@@ -18,73 +17,31 @@ LSM303 compass;
 
 #define K_DELAY 50  // sensor loop delay
 
+const uint8_t K_MODE_IDLE = 0;
+const uint8_t K_MODE_TEST = 1;
+const uint8_t K_MODE_ROAM = 2;
+uint8_t MODE = K_MODE_IDLE;
 
 char report[120];
 
-int16_t acc_x_level = 0;
-int16_t acc_y_level = 0;
-int16_t acc_z_level = 0;
-
-# define MAX_CT 3
-int16_t acc_x_max[MAX_CT];
-uint8_t acc_x_max_i = 0;
-int16_t acc_y_max[MAX_CT];
-uint8_t acc_y_max_i = 0;
-int16_t acc_z_max[MAX_CT];
-uint8_t acc_z_max_i = 0;
 
 
-
-void buff_flush(int16_t* arr)
-{
-  uint8_t i = 0;
-  while (i < MAX_CT)
-  {
-    arr[i] = 0;
-    i++;
-  }
-}
-
-int16_t buff_update_med(int16_t* arr, uint8_t* arr_i, int16_t a)
-{
-  // insert in buffer
-  arr[acc_z_max_i] = a;
-  (*arr_i)++;
-  if (*arr_i == MAX_CT)
-  {
-    *arr_i = 0;
-  }
-  
-  // find max and min of buffer
-  int16_t amax = -32768;
-  int16_t amin = 32767;
-  uint8_t i = 0;
-  while (i < MAX_CT)
-  {
-    if (arr[i] >= amax)
-    {
-      amax = arr[i];
-    }
-    if (arr[i] <= amin)
-    {
-      amin = arr[i];
-    }
-    i++;
-  }
-  return amax + amin;
-}
-
-
+//---------------------------------------------------
+// ACCELEROMETER CALIBRATION
 
 #define K_CALIB_CT  80
 #define K_CALIB_DIV (K_CALIB_CT / 7)  // display 7 dots (+1 final dot)
+int16_t acc_x_level = 0;
+int16_t acc_y_level = 0;
+int16_t acc_z_level = 0;
 
 void calib()
 {
   int16_t n = 1;
   lcd.clear();
   lcd.print("Calib.");  
-  lcd.gotoXY(0, 1);  
+  lcd.gotoXY(0, 1);
+  delay(500);  
 
   while (n < K_CALIB_CT)
   {
@@ -117,34 +74,52 @@ void calib()
 
 
 
+//---------------------------------------------------
+// TEST MODE
+
+int8_t test_acc_justify(int16_t i)
+{
+  int8_t w = 3;
+  int16_t abs_i = abs(i);
+  if (abs_i >= 10) w--;
+  if (abs_i >= 100) w--;
+  if (i < 0) w--;
+  return w;
+}
+
 void test_acc()
 {
   lcd.clear();
-  lcd.print("TestAcc!");
-
-  buff_flush(acc_x_max);
-  buff_flush(acc_y_max);
-  buff_flush(acc_z_max);
-
+  lcd.print("  AX  AY");
   while (1)
   {
     compass.read();
-    int16_t diff_x = compass.a.x - acc_x_level;
-    int16_t diff_y = compass.a.y - acc_y_level;
-    int16_t diff_z = compass.a.z - acc_z_level;
-    int16_t med_x = buff_update_med(acc_x_max, &acc_x_max_i, diff_x);
-    int16_t med_y = buff_update_med(acc_y_max, &acc_y_max_i, diff_y);
-    int16_t med_z = buff_update_med(acc_z_max, &acc_z_max_i, diff_z);
-    
+    int16_t acc_x = compass.a.x - acc_x_level;
+    int16_t acc_y = compass.a.y - acc_y_level;
+#if 0
+    // full accelerometer data can be sent to serial monitor  
+    int16_t acc_z = compass.a.z - acc_z_level;
     snprintf_P(report, sizeof(report),
       PSTR("A: %6d %6d %6d"),
-      med_x, med_y, med_z);
+      acc_x, acc_y, acc_z);
     Serial.println(report);
+#endif
+    acc_x = constrain(acc_x / 100, -99, 99);
+    acc_y = constrain(acc_y / 100, -99, 99);
+    lcd.gotoXY(0, 1);
+    lcd.print("        ");
+    lcd.gotoXY(0 + test_acc_justify(acc_x), 1);
+    lcd.print(acc_x);
+    lcd.gotoXY(4 + test_acc_justify(acc_y), 1);
+    lcd.print(acc_y);
     delay(100);    
   }
 }
 
 
+
+//---------------------------------------------------
+// MOTOR RAMPING
 
 #define K_RAMP_SAMPLES 11
 
@@ -152,7 +127,6 @@ void test_acc()
 // acceleration scale factors ramp the velocity
 // scale factor is 0-1 with a 10x multiplier in table
 int16_t k_acc_fac[K_RAMP_SAMPLES] = {0,3,7,10,7,3,0,0,0,0,0};
-
 uint8_t k_ramp = 0;
 int16_t k_toggle = 0;
 
@@ -176,20 +150,51 @@ void ramp_motors(int16_t vmax)
 
 
 
+// return true if expr is true for max counts
+bool is_ct_reached(bool expr, uint8_t* pct, uint8_t max)
+{
+  bool result = false;
+  if (expr)
+  {
+    (*pct)++;
+    if (*pct >= max)
+    {
+      result = true;
+    }
+  }
+  else
+  {
+    *pct = 0;
+  }
+  return result;
+}
+
+
+
+//---------------------------------------------------
+// ROAM MODE
+
 const uint8_t E_FLAT = 0;
 const uint8_t E_DOWN = 1;
 const uint8_t E_SPIN = 2;
 
+const int16_t K_ACC_PT_DOWN = -3000;
+const uint8_t K_FLAT_CT = 3;
+const uint8_t K_DOWN_CT = 3;
+const uint8_t K_SPIN_CT = 3;
+
 void roam()
 {
-  lcd.clear();
-  lcd.print("Roaming!");
-  delay(500);
-
   uint8_t state = E_FLAT;
   int16_t vmax = 200;
   int16_t vspin = 200;
+  uint8_t ct = 0;
+  bool flag = false;
+
+  lcd.clear();
+  lcd.print("Roaming!");
   ledGreen(1);
+  delay(500);
 
   while (1)
   {
@@ -199,29 +204,30 @@ void roam()
     // read sensors and apply calibration offsets
     // then update median filters
     compass.read();
-    int16_t diff_x = compass.a.x - acc_x_level;
-    int16_t diff_y = compass.a.y - acc_y_level;
-    int16_t med_x = buff_update_med(acc_x_max, &acc_x_max_i, diff_x);
-    int16_t med_y = buff_update_med(acc_y_max, &acc_y_max_i, diff_y);
+    int16_t acc_x = compass.a.x - acc_x_level;
+    int16_t acc_y = compass.a.y - acc_y_level;
 
     switch (state)
     {
       case E_FLAT:
         // normal flat motion moving one motor at a time
         ramp_motors(vmax);
-        if (med_x < -3000)
+        flag = (acc_x < K_ACC_PT_DOWN);
+        if (is_ct_reached(flag, &ct, K_FLAT_CT))
         {
           // robot started pointing downhill
           ledGreen(0);
           ledRed(1);
           state = E_DOWN;
+          ct = 0;
         }
         break;
       case E_DOWN:
         // continue ramping in reverse direction
         // to undo whatever motion made robot point downhill
         ramp_motors(-vmax);
-        if ((med_x > -1200) && (k_ramp == 0))
+        flag = (acc_x > -1200);
+        if (is_ct_reached(flag, &ct, K_DOWN_CT) && (k_ramp == 0))
         {
           // robot has stopped its ramp motion
           // and is not pointing downhill as much
@@ -244,12 +250,14 @@ void roam()
           motors.setSpeeds(vspin/2, -vspin/2);
           delay(250);
           state = E_SPIN;
+          ct = 0;
         }
         break;
       case E_SPIN:
         // keep spinning until pointing uphill again
         // TODO -- or iteration limit is reached ?
-        if ((med_y < 500) && (med_y > -500) && (med_x > 0))
+        flag = ((acc_y < 500) && (acc_y > -500) && (acc_x > 0));
+        if (is_ct_reached(flag, &ct, K_SPIN_CT))
         {
           ledYellow(0);
           ledGreen(1);
@@ -257,6 +265,7 @@ void roam()
           // let everything settle before resuming roam
           delay(500);
           state = E_FLAT;
+          ct = 0;
         }
         break;
       default:
@@ -267,12 +276,19 @@ void roam()
 
 
 
+//---------------------------------------------------
+// MAIN ARDUINO FUNCTIONS
+
 void setup()
 {
+  uint8_t ct_a = 0;
+  uint8_t ct_b = 0;
+  
   Wire.begin();
   ledYellow(0);
   ledGreen(0);
   ledRed(0);
+  lcd.clear();
 
   if (!compass.init())
   {
@@ -280,28 +296,56 @@ void setup()
     ledRed(1);
     while(1)
     {
-      Serial.println(F("Failed to detect the compass."));
-      delay(100);
+      lcd.print("COMP ERR");
+      delay(500);
     }
   }
 
   compass.enableDefault();
 
   // display app title and wait for button press
+  // prompt for A but B will enter "secret" test mode
   lcd.clear();
   lcd.print("Hillroam");
   lcd.gotoXY(0, 1);  
   lcd.print("Press A.");
-  buttonA.waitForButton();
-  delay(500);  
+  while (1)
+  {
+    delay(10);
+    if (buttonA.getSingleDebouncedPress())
+    {
+      while (!buttonA.getSingleDebouncedRelease())
+      {
+        delay(10);
+      }
+      MODE = K_MODE_ROAM;
+      break;
+    }
+    if (buttonB.getSingleDebouncedPress())
+    {
+      ledYellow(1);
+      while (!buttonB.getSingleDebouncedRelease())
+      {
+        delay(10);
+      }
+      MODE = K_MODE_TEST;
+      break;
+    }
+  }
 }
-
-
 
 void loop()
 {
+  // always calibrate accelerometers
+  // then enter either test mode or normal roaming mode
   calib();
-  //test_acc();
-  roam();
+  if (MODE == K_MODE_TEST)
+  {
+    test_acc();
+  }
+  else
+  {
+    roam();
+  }
 }
 
